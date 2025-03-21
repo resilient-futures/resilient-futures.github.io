3dcbe979006e5e910e730663daefa522b89b7729
taraliu23 <rainylty@gmail.com>
2025-03-21 15:43:18 -0400

    update: team, position

--- yiyihe-page/src/pages/Team.jsx
+++ yiyihe-page/src/pages/Team.jsx
@@ -6,6 +6,7 @@ const teamData = {
 	principalInvestigator: [
 		{
 			name: 'Yiyi He',
+			position: 'Assistant Professor',
 			bio: 'Yiyi He is an assistant professor in the School of City and Regional Planning (SCaRP) at the College of Design at Georgia Tech. Her research centers on the interdisciplinary fields of urban planning, GIScience, climate science, and artificial intelligence. She is interested in building a better understanding of the uncertainty and asymmetric impacts of climate-change-induced extreme weather events (e.g., flooding, wildfires, extreme heat) on critical components of the built environment (e.g., lifeline infrastructure networks, vulnerable neighborhoods). She leverages data-driven approaches, such as GIS, network science, hyperspectral remote sensing, machine learning, and spatial statistics to tackle complex challenges in climate change and resilience research and to inform more intelligent planning and policy directives.',
 			image: '/team-member/yiyihe.png',
 			linkedin: 'https://www.linkedin.com/in/yiyihe',
@@ -16,6 +17,7 @@ const teamData = {
 	currentMembers: [
 		{
 			name: 'Tara Liu',
+			position: 'Graduate Research Assistant',
 			bio: 'Tara(they/their) is a master student in CSE and an aspiring data scientist. They are interested in empowering the marginalized communities with technology and have 10+ industry experience. They enjoy nature and metal music in free time. Fun fact: Taco cat spelled backward is taco cat.',
 			image: '/team-member/taraliu.jpg',
 			linkedin: 'https://www.linkedin.com/in/tara-tingyu-liu/',
@@ -24,6 +26,7 @@ const teamData = {
 		},
 		{
 			name: 'Yuehan Zhang',
+			position: 'Graduate Teaching Assistant',
 			bio: 'Yuehan Zhang is a Master student in Computational Science & Engineering at Georgia Institute of Technology, with a bachelor degree in Theoretical and Applied Mechanics from Fudan University. She has worked on projects involving transportation analytics and fine-tuning large language models. With experience in model optimization and hardware-software co-design, she is passionate about leveraging data-driven methods to solve complex, real-world problems.',
 			image: '/team-member/yuehan.jpg',
 			linkedin: 'http://linkedin.com/in/yuehan-zhang-9676bb189',
@@ -32,6 +35,7 @@ const teamData = {
 		},
 		{
 			name: 'Gabriel Appiah',
+			position: 'PhD student',
 			bio: 'Gabriel Appiah is a Ph.D. student at the School of City and Regional Planning. Before joining the program, he worked as a Regional Planner II at the Siouxland Interstate Metropolitan Planning Council in Iowa. His broader research interests include GIS, urban planning, and data science. His dissertation examines GIS development disparities worldwide, specifically focusing on inequalities in access to geospatial data and GIS education. Dr. Clio Andris and Dr. Yiyi He serve as his advisors. Gabriel holds an M.A. in Urban and Regional Studies from Minnesota State University and a B.S. in Development Planning from Kwame Nkrumah University of Science and Technology in Ghana.',
 			image: '/team-member/gabrielappiah.jpg',
 			linkedin: 'http://linkedin.com/in/yuehan-zhang-9676bb189',
@@ -40,6 +44,7 @@ const teamData = {
 		},
 		{
 			name: 'Tiger Peng',
+			position: 'Graduate Research Assistant',
 			bio: 'Tiger is pursuing a master’s degree in computer science at Georgia Tech with a concentration in machine learning. He will graduate in May 2026. His interests lie at the intersection of computing, data, and sustainability. He has pursued this passion through an internship at the solar company Silicon Ranch Corporation, where he performed data analysis and predictive modeling, and a research assistantship with the Georgia Tech School of City and Regional Planning, where he studied the relationship between weather and power grid stability in India.',
 			image: '/team-member/tigerpeng.jpg',
 			linkedin: ' https://www.linkedin.com/in/pengtiger/',
@@ -52,6 +57,7 @@ const teamData = {
 		{
 			name: 'Changmin Seok',
 			bio: 'Changmin is a second-year master student in city and regional planning.His research focuses on transportation economics and economic development in emerging cities of the Global South.Before joining Tech, Changmin was a loan officer at the Export- Import Bank of Korea, where he focused on deploying feasibility studies and due diligence for infrastructure projects in the developing countries.',
+			position: 'Graduate Research Assistant',
 			image: '/team-member/changminseok.jpg',
 			linkedin: 'www.linkedin.com/in/changmin-seok-63a782b1',
 			github: 'https://github.com/changmin-seok',
@@ -62,12 +68,13 @@ const teamData = {
 	]
 }
 
-const TeamMember = ({ name, bio, image, linkedin, github, website }) => {
+const TeamMember = ({ name, position, bio, image, linkedin, github, website }) => {
 	return (
 		<div className="team-member">
 			<img src={image} alt={name} className="team-member__image" />
 			<div className="team-member__info">
 				<h3>{name}</h3>
+				<h4>{position}</h4>
 				<p>{bio}</p>
 				{linkedin && <a href={linkedin} target="_blank" rel="noopener noreferrer">[Linkedin]</a>}
 				&nbsp;
